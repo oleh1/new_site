c0fa3dab9f21d57acc745e08c24915920ee4a491
oleh1 <lyboleg@gmail.com>
2018-01-23 22:35:34 +0200

    1

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<div>
+    <div style="margin: 0 0 0 25%;width: 50%;">
+        <div>Rostislav Malimonenko</div>
+        <div>Barrister, attorney, lawyer, advocate of Ukraine</div>
+    </div>
+</div>
+
+<div>
+    <div style="margin: 0 0 0 25%;width: 50%;">
+        <div></div>
+        <div>Barrister, attorney, lawyer, advocate of Ukraine</div>
+        <div>Barrister Rostislav Malimonenko, the member of Ukrainian National BAR Association will help to resolve any legal issues in Ukraine including representation in criminal cases, recovering a debt, divorce, determination of the child's place of residence through the court in Ukraine, division of spouses' property in Ukraine , representation and protection in Ukraine in all disputable cases on the territory of Ukraine. malimonenko@ukr.net +380667371277 +380993090829</div>
+    </div>
+</div>
+
+<div>
+    <div style="margin: 0 0 0 25%;width: 50%;">
+        <div>I live and work as a barrister in Ukraine. I have a lot of experience in all criminal cases, labour cases, civil cases, matrimonial cases, corporate cases, administrative proceedings, as well as other cases solution of which is required by the client.</div>
+        <div>I have a lot of practical experience in appealing against illegal actions of police officers, prosecutors, and other officials.</div>
+        <div>I always glad to help in solving your problems in Ukraine and protecting your rights and interests.</div>
+    </div>
+</div>
+
+<div>
+    <div></div>
+    <div></div>
+</div>
+
+<div>
+    <div></div>
+    <div></div>
+</div>
+
+</body>
+</html>
