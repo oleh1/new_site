db4c6f5cd81614d9742efd414e1d98e7a60c1787
oleh1 <lyboleg@gmail.com>
2018-01-23 23:07:52 +0200

    2

--- index.php
+++ index.php
@@ -33,14 +33,233 @@
 </div>
 
 <div>
-    <div></div>
-    <div></div>
+    <div style="margin: 0 0 0 25%;width: 50%;">
+        <div style="width: 50%;display: inline-block;">
+            <div>CONTACTS</div>
+            <div></div>
+            <div>
+                <div>
+                    <div>Telephone</div>
+                    <div>+380667371277, +380993090829</div>
+                </div>
+                <div>
+                    <div>Email</div>
+                    <div>malimonenko@ukr.net</div>
+                </div>
+            </div>
+        </div>
+
+        <div style="width: 49%;display: inline-block;background-color: rgb(187, 200, 221);">
+            <div style="letter-spacing:0.1em;color: #070A0E;word-wrap: break-word;font: normal normal bold 38px/1.4em 'open sans', sans-serif;color: #070A0E;">WRITE ME</div>
+            <div style="width: 52px; height: 7px;border-bottom: 7px solid rgba(223, 49, 67, 1);height: 0 !important;min-height: 0 !important;margin: 0 0 15px 0;"></div>
+            <div>
+
+                <div class="cf1" data-state="   field4Hidden field5Hidden  desktop left" id="comp-it43dhlq" data-dcf-columns="3">
+                    <form role="form" aria-label="contact form" novalidate="" id="comp-it43dhlqform-wrapper" class="cf1form-wrapper">
+                        <div id="comp-it43dhlqwrapper" class="cf1wrapper">
+                            <div class="cf1_first-row-wrapper"><input type="text" id="field1" required="" aria-invalid="false" name="Имя" value="" class="cf1_required" placeholder="Name" data-aid="nameField"><input type="text" id="field2" required="" aria-invalid="false" name="Email" value="" class="cf1_required" placeholder="Email" data-aid="emailField">
+                            </div>
+                            <div><input type="text" id="field3" aria-invalid="false" name="Тема" value="" class="" placeholder="Theme" data-aid="subjectField"><input type="tel" id="field4" aria-invalid="false" name="Телефон" value="" class="cf1_hidden cf1_hiddenField" placeholder="Телефон" data-aid="phoneField"><input type="text" id="field5" aria-invalid="false" name="Адрес" value="" class="cf1_hidden cf1_hiddenField" placeholder="Адрес" data-aid="addressField">
+                            </div>
+                            <textarea placeholder="Message" name="Сообщение" class="cf1fieldMessage" data-aid="messageField" id="comp-it43dhlqfieldMessage"></textarea>
+                            <div id="comp-it43dhlqsubmit1" style="font-size:  25px;font-weight:  bold;" class="cf1submit1">Send
+                            </div>
+                            <div aria-live="polite" class="cf1_success cf1notifications" id="comp-it43dhlqnotifications"></div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </div>
 
 <div>
     <div></div>
     <div></div>
 </div>
+
+<style type="text/css">
+    /*form*/
+    .cf1[data-state~="left"] {
+        direction: ltr;
+        text-align: left;
+    }
+    body {
+        font-size: 10px;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    a, abbr, acronym, address, applet, b, big, blockquote, body, button, caption, center, cite, code, dd, del, dfn, div, dl, dt, em, fieldset, font, footer, form, h1, h2, h3, h4, h5, h6, header, html, i, iframe, img, ins, kbd, label, legend, li, nav, object, ol, p, pre, q, s, samp, section, small, span, strike, strong, sub, sup, table, tbody, td, tfoot, th, thead, title, tr, tt, u, ul, var {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        outline: 0;
+        vertical-align: baseline;
+        background: transparent;
+    }
+    .cf1wrapper {
+        min-width: 180px;
+        max-width: 980px;
+        width: 100%;
+        text-align: center;
+    }
+    .cf1_first-row-wrapper {
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: flex;
+    }
+    .cf1_first-row-wrapper input:first-child {
+        margin-right: 30px;
+    }
+    .cf1 input {
+        margin-bottom: 12px;
+    }
+    .cf1 input, .cf1 textarea {
+        padding: 5px;
+        border: none;
+        border-bottom: 1px solid rgba(46, 73, 117, 1);
+        color: #FFFFFF;
+        margin: 0 0 5px;
+        width: 100%;
+        -webkit-transition: 0.5s ease all;
+        transition: 0.5s ease all;
+        background-color: transparent;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        font: normal normal normal 17px/1.4em 'open sans', sans-serif;
+        -webkit-appearance: none;
+        border-radius: 0;
+    }
+    .cf1_first-row-wrapper input {
+        -webkit-box-flex: 1;
+        -webkit-flex: 1;
+        flex: 1;
+        min-width: 0;
+    }
+    input, select, textarea {
+        font-family: Helvetica, Arial, sans-serif;
+        box-sizing: border-box;
+    }
+    .cf1 input {
+        margin-bottom: 12px;
+    }
+    .cf1 input, .cf1 textarea {
+        padding: 5px;
+        border: none;
+        border-bottom: 1px solid rgba(46, 73, 117, 1);
+        color: #FFFFFF;
+        margin: 0 0 5px;
+        width: 100%;
+        -webkit-transition: 0.5s ease all;
+        transition: 0.5s ease all;
+        background-color: transparent;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        font: normal normal normal 17px/1.4em 'open sans', sans-serif;
+        -webkit-appearance: none;
+        border-radius: 0;
+    }
+    .cf1_first-row-wrapper input {
+        -webkit-box-flex: 1;
+        -webkit-flex: 1;
+        flex: 1;
+        min-width: 0;
+    }
+    input, select, textarea {
+        font-family: Helvetica, Arial, sans-serif;
+        box-sizing: border-box;
+    }
+    .cf1 textarea {
+        resize: none;
+        overflow: hidden;
+        margin: 0 0 10px;
+    }
+    .cf1 input, .cf1 textarea {
+        padding: 5px;
+        border: none;
+        border-bottom: 1px solid rgba(46, 73, 117, 1);
+        color: #FFFFFF;
+        margin: 0 0 5px;
+        width: 100%;
+        -webkit-transition: 0.5s ease all;
+        transition: 0.5s ease all;
+        background-color: transparent;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        font: normal normal normal 17px/1.4em 'open sans', sans-serif;
+        -webkit-appearance: none;
+        border-radius: 0;
+    }
+    .cf1fieldMessage {
+        min-height: 128px;
+    }
+    input, select, textarea {
+        font-family: Helvetica, Arial, sans-serif;
+        box-sizing: border-box;
+    }
+    .cf1submit1 {
+        color: #2E4975;
+        font: normal normal normal 17px/1.4em 'open sans', sans-serif;
+    }
+    .cf1notifications.cf1_success {
+        color: #8680E0;
+    }
+    .cf1notifications {
+        font: normal 14px 'Helvetica Neue', Helvetica, sans-serif;
+        margin-top: 20px;
+        height: 14px;
+    }
+    .cf1 input {
+        margin-bottom: 12px;
+    }
+    .cf1 input, .cf1 textarea {
+        padding: 5px;
+        border: none;
+        border-bottom: 1px solid rgba(46, 73, 117, 1);
+        color: #FFFFFF;
+        margin: 0 0 5px;
+        width: 100%;
+        -webkit-transition: 0.5s ease all;
+        transition: 0.5s ease all;
+        background-color: transparent;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        font: normal normal normal 17px/1.4em 'open sans', sans-serif;
+        -webkit-appearance: none;
+        border-radius: 0;
+    }
+    input, select, textarea {
+        font-family: Helvetica, Arial, sans-serif;
+        box-sizing: border-box;
+    }
+    .cf1 input {
+        margin-bottom: 12px;
+    }
+    .cf1 input, .cf1 textarea {
+        padding: 5px;
+        border: none;
+        border-bottom: 1px solid rgba(46, 73, 117, 1);
+        color: #FFFFFF;
+        margin: 0 0 5px;
+        width: 100%;
+        -webkit-transition: 0.5s ease all;
+        transition: 0.5s ease all;
+        background-color: transparent;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        font: normal normal normal 17px/1.4em 'open sans', sans-serif;
+        -webkit-appearance: none;
+        border-radius: 0;
+    }
+    .cf1_hiddenField {
+        display: none !important;
+    }
+    input, select, textarea {
+        font-family: Helvetica, Arial, sans-serif;
+        box-sizing: border-box;
+    }
+    /*form*/
+</style>
 
 </body>
 </html>
