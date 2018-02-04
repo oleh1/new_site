b7c1fff540506e80a877e660b8161caa53e1b177
oleh1 <lyboleg@gmail.com>
2018-02-04 10:51:33 +0200

    statistic 2

--- index.php
+++ index.php
@@ -24,6 +24,28 @@
         </script>
     <?php } ?>
 
+    <?php if($_SERVER['HTTP_HOST'] == 'advocate-of-ukraine.pp.ua'){ ?>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113384561-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-113384561-2');
+        </script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113449708-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-113449708-2');
+        </script>
+    <?php } ?>
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
